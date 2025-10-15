913429b0964adadc9b54ef0d126dd19351d293c7
ARCAL <l22121351@morelia.tecnm.mx>
2025-10-15 13:30:00 -0600

    (fix) prefix api/v1 added to services routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
+
 from fastapi import FastAPI
 from app.api.auth.auth_router import router as auth_router
 from app.api.products.product_router import router as product_router
 from app.api.ingredients.ingredient_router import router as ingredient_router
+from app.core.settings import settings
 
 app = FastAPI(
     title="CoffeeShop API",
@@ -10,9 +12,9 @@ app = FastAPI(
 )
 
 # Incluir routers
-app.include_router(auth_router)
-app.include_router(product_router)
-app.include_router(ingredient_router)
+app.include_router(auth_router, prefix=settings.API_V1)
+app.include_router(product_router, prefix=settings.API_V1)
+app.include_router(ingredient_router, prefix=settings.API_V1)
 
 @app.get("/")
 def read_root():
