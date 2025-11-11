59bc1442b854743e814cb1361d17fc853a04326e
Cesar Figueroa <cesarfig117@gmail.com>
2025-11-10 18:18:07 -0600

    feat(CS-102): Change password done

--- app/utils/email.py
+++ app/utils/email.py
@@ -6,7 +6,7 @@ from app.core.settings import settings
 
 from app.core.http_response import CoffeeAppHttpResponse
 
-from app.constants.email_template import new_user_verification_code_email_tempalte
+from app.constants.email_template import new_user_verification_code_email_tempalte, new_password_reset_code_email_template
 
 
 class EmailService:
@@ -101,3 +101,50 @@ class EmailService:
 
         finally:
             mail.quit()
+
+    @staticmethod
+    async def send_password_reset_code_email(
+        to_name: str, to_email: str, verification_code: str
+    ):
+        smtp_server = settings.SMTP_SERVER
+        smtp_port = settings.SMTP_PORT
+        smtp_username = settings.SMTP_USERNAME
+        smtp_password = settings.SMTP_PASSWORD
+        verification_code = verification_code
+
+        msg = MIMEMultipart("alternative")
+        msg["From"] = smtp_username
+        msg["To"] = to_email
+        msg["Subject"] = "Codigo de reestablecimiento de contraseña."
+
+        text = """¡Activa tu cuenta ahora!"""
+
+        html = new_password_reset_code_email_template(
+            user_name=to_name, code=verification_code
+        )
+
+        try:
+            first_part = MIMEText(text, "plain")
+            second_part = MIMEText(html, "html")
+
+            msg.attach(first_part)
+            msg.attach(second_part)
+
+            mail = smtplib.SMTP(smtp_server, smtp_port)
+
+            mail.ehlo()
+
+            mail.starttls()
+
+            mail.login(smtp_username, smtp_password)
+            mail.sendmail(smtp_username, to_email, msg.as_string())
+
+        except smtplib.SMTPException:
+            CoffeeAppHttpResponse.internal_error()
+
+        except Exception:
+            CoffeeAppHttpResponse.internal_error()
+
+        finally:
+            mail.quit()
+
