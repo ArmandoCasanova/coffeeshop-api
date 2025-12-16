fb43694719d6edeb2e10ab72ad357309d96de584
ARCAL <l22121351@morelia.tecnm.mx>
2025-12-15 21:04:50 -0600

    (fix) espacio

--- app/api/auth/auth_controller.py
+++ app/api/auth/auth_controller.py
@@ -125,6 +125,7 @@ class AuthController:
             raise
         except Exception:
             CoffeeAppHttpResponse.internal_error()
+            
     
     async def get_current_user(self, email: str) -> UserModel:
         try:            
